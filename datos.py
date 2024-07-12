198aa25bd126ec3c0b622d2387c45885b69e02ea
DLeonardoG <darkleonardog@gmail.com>
2024-07-12 15:18:43 -0500

    feat: :sparkles: nuevo aarchico crud
    se añadieron archivos y funcionalidades en proceso para el crud
    
    @ by Leo

--- datos.py
+++ datos.py
@@ -5,12 +5,13 @@ def cargar_datos(archivo):
     with open(archivo,"r") as file:
         datos=json.load(file)
     return datos
-        
-        
 
 def guardar_datos(datos, archivo):
     datos = dict(datos)
-    diccionario=json.dumps(datos, indent=4)
-    file=open(archivo,"w")
-    file.write(diccionario)
-    file.close()
+    
+    diccionario = json.dumps(datos, indent=4)
+    with open(archivo, "w", encoding="utf-8") as file:
+        file.write(diccionario)
+    
+RUTA_BASE_DE_DATOS ="ciudades.json"
+datos = cargar_datos(RUTA_BASE_DE_DATOS)
